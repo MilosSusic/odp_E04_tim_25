5b2d2c994e574dc7df7a93a8cfe89de0095e032c
MilosSusic <milossusic22@gmail.com>
2025-08-26 18:15:31 +0200

    jos jedna

--- DB_Upiti.sql
+++ DB_Upiti.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS DEFAULT_DB;
 
 -- Koriscenje default baze podataka
 USE DEFAULT_DB;
-
+--ovo je komentar
 -- Kreiranje tabele za korisnike
 CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY AUTO_INCREMENT,
